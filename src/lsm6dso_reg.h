0d3066e8ec6e6659bc71d2eb9cfa74687977f6d6
Carlo Parata <carlo.parata@st.com>
2020-11-03 17:13:06 +0100

    Fix conflict issue with LSM6DSOX PID

--- src/lsm6dso_reg.h
+++ src/lsm6dso_reg.h
@@ -2880,8 +2880,8 @@ int32_t lsm6dso_all_sources_get(lsm6dso_ctx_t *ctx,
 
 typedef struct{
   uint8_t odr_fine_tune;
-} dev_cal_t;
-int32_t lsm6dso_calibration_get(lsm6dso_ctx_t *ctx, dev_cal_t *val);
+} lsm6dso_dev_cal_t;
+int32_t lsm6dso_calibration_get(lsm6dso_ctx_t *ctx, lsm6dso_dev_cal_t *val);
 
 typedef struct {
   struct {
